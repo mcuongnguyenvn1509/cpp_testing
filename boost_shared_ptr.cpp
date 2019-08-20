b463859d55c43553508a08947b28c30ddebd9ab4
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-08-20 18:11:09 +0700

    Commit for dynamic_pointer_cast from Base to derived_drrived

--- boost_shared_ptr.cpp
+++ boost_shared_ptr.cpp
@@ -1,16 +1,17 @@
-//source from https://www.boost.org/doc/libs/1_34_0/libs/smart_ptr/test/pointer_cast_test.cpp
 #include <boost/shared_ptr.hpp>
 #include <boost/detail/lightweight_test.hpp>
+//#include <iostream>
+using namespace std;
 namespace
 {
 
 // Let's create these inheritance relationship:
 //
-//    Base   Base2
+//    base   base2
 //      |     |
-//      Derived
+//      derived
 //         |
-//  Derived_derived
+//  derived_derived
 //
 
 class Base
@@ -18,6 +19,9 @@ class Base
    public:
    virtual ~Base(){}
    int filler [5];
+   void run(){
+      std::cout << "This is the Base" <<std::endl; 
+   }
 };
 
 class Base2
@@ -26,18 +30,32 @@ public:
 
     virtual ~Base2(){}
     int filler [5];
+   void run(){
+      std::cout << "This is the Base 2" <<std::endl;
+   }
+
 };
 
 class Derived
-   : public Base, public Base2
+   : public Base, public Base2 
 {
+public:
     int filler [5];
+   void run(){
+      std::cout << "This is the Derived" <<std::endl;
+   }
+
 };
 
 class Derived_derived
    : public Derived
 {
+public:
     int filler [5];
+   void run(){
+      std::cout << "This is the Derived_derived" <<std::endl;
+   }
+
 };
 
 // And now some simple check functions
@@ -50,7 +68,7 @@ bool check_dynamic_pointer_cast(const BasePtr &ptr)
    //Correct cast with dynamic_pointer_cast
    boost::get_pointer(boost::dynamic_pointer_cast<Derived>(ptr)) ==
       //Correct cast with dynamic_cast
-      dynamic_cast<Derived*>(boost::get_pointer(ptr))
+      dynamic_cast<Derived*>(boost::get_pointer(ptr)) 
    &&
    //Incorrect cast with dynamic_pointer_cast
    boost::get_pointer(boost::dynamic_pointer_cast<Derived_derived>(ptr)) ==
@@ -66,12 +84,20 @@ int main()
     {
         // Try casts with shared_ptr
 
-        boost::shared_ptr<Base> ptr(new Derived);
-
+        boost::shared_ptr<Derived> ptr(new Derived);
+        //boost::shared_ptr<Base> ptr1(new Base);
+        boost::shared_ptr<Base> ptr1(new Derived_derived);
+        boost::shared_ptr<Derived_derived> ptr2(new Derived_derived);
+        ptr1-> run();
+        ptr-> run();
+        ptr2-> run();
+        boost::shared_ptr<Derived_derived> ptr3 = boost::dynamic_pointer_cast<Derived_derived>(ptr1);
+        ptr3-> run(); 
         BOOST_TEST( check_dynamic_pointer_cast( ptr ) );
+        BOOST_TEST( check_dynamic_pointer_cast( ptr1 ) );
 
     }
 
-
+    
     return boost::report_errors();
 }
