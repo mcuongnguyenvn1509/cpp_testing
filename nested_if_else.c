d77694422e0bf11f4564c4951a493b577ce6476c
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-10-28 22:50:12 +0700

    Update nested_if_else.c

--- nested_if_else.c
+++ nested_if_else.c
@@ -25,3 +25,38 @@ int main()
    }
    return 0;
 }
+
+
+#include<stdio.h>
+
+int main()
+{
+	 int num1, num2, num3;
+	 printf("Enter three numbers:\n");
+	 scanf("%d%d%d",&num1, &num2, &num3);
+	 if(num1>num2)
+	 {
+		  /* This is nested if-else */
+		  if(num1>num3)
+		  {
+		   	printf("Largest = %d", num1);
+		  }
+		  else
+		  {
+		   	printf("Largest = %d", num3);
+		  }
+	 }
+	 else
+	 {
+		  /* This is nested if-else */
+		  if(num2>num3)
+		  {
+		   	printf("Largest = %d", num2);
+		  }
+		  else
+		  {
+		   	printf("Largest = %d", num3);
+		  }
+	 }
+	 return(0);
+}
