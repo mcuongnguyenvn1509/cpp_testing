efe1c20c7be0baeaf550f66eb6870feb68b0c973
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2020-01-21 23:11:06 +0700

    Create 01_define_a_class.cpp

---
+++ 01_define_a_class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class class_name
+{
+private:
+
+//declaration of variables
+//declaration of functions
+
+protected:
+//declaration of variables
+//declaration of functions
+
+public:
+//declaration of variables
+//declaration of functions
+
+};
