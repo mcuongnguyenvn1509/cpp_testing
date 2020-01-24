e1acd694f706934dc02338d61327f98e67a9b1cd
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2020-01-24 16:58:30 +0100

    Create 01_constructor.cpp

---
+++ _Google_unittest/01_constructor.cpp
@@ -0,0 +1,42 @@
+#include "01_constructor.h"
+
+Constructor::Constructor(){
+        _s_copy = NULL;
+
+}
+
+Constructor::Constructor(char* str){
+        _s_copy = new char[16];
+        strcpy(_s_copy, str._s_copy);
+}
+
+Constructor::Constructor(const Constructor& str){
+        this->_s_copy= NULL;
+        if(str._s_copy != NULL){
+                _s_copy= new char [16] //Dynamic memory alocation
+                strcpy(_s_copy, str._s_copy);
+        }
+
+
+}
+
+Constructor& Constructor::operator=(const Constructor& str){
+        if(this == &str){
+                return *this;
+        }
+        else{
+                char* tmp_s_copy = new char[16];
+                delete []_s_copy;
+                _s_copy= tmp_s_copy;
+                strcpy(_s_copy,str._s_copy);
+
+        }
+        return *this;
+}
+
+Constructor::~Constructor(){
+        if(_s_copy != NULL){
+                delete []_s_copy;
+        }
+
+}
