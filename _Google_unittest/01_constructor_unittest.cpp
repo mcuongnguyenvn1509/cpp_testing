f48bb165e7960a241aad4ffda528141a9421140a
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2020-01-24 16:59:44 +0100

    Create 01_constructor_unittest.cpp

---
+++ _Google_unittest/01_constructor_unittest.cpp
@@ -0,0 +1,13 @@
+#include "01_constructor.h"
+#include "gtest/gtest.h"
+
+namespace {
+        TEST(MyConstructor, DefaultConstructor){
+                const Constructor c;
+                EXPECT_STREQ(nullptr, c.
+        }
+
+
+
+
+}
