81e97b0f3effa737a74f3d8dda670ea155f39adf
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2020-01-27 17:46:27 +0100

    Update 01_constructor.cpp

--- _Google_unittest/01_constructor.cpp
+++ _Google_unittest/01_constructor.cpp
@@ -2,23 +2,48 @@
 
 Constructor::Constructor(){
         _s_copy = NULL;
+	_sId = 0;
+	_sLength = 16;
 
+}
+
+Constructor::Constructor(char sId, int sLength){
+	_s_copy= NULL;
+	_sId = sId;
+	_sLength = sLength;
+        _s_copy = new char[_sLength];
 }
 
 Constructor::Constructor(const char* str){
 	_s_copy=NULL;
-        _s_copy = new char[16];
-        strcpy(_s_copy, str);
+	_sId = 0;
+	_sLength = 16;
+	if(str != NULL){
+        	_s_copy = new char[_sLength];
+	        //strcpy(_s_copy, str);
+		std::memcpy( _s_copy, str, _sLength);
+	}
+}
+
+Constructor::Constructor(char* str, int sLength){
+	_s_copy=NULL;
+	_sId = 0;
+	_sLength = sLength;
+	if(str != NULL){
+	        _s_copy = new char[_sLength];
+        	//strcpy(_s_copy, str);
+	        std::memcpy( _s_copy, str, _sLength);
+	}
 }
 
 Constructor::Constructor(const Constructor& str){
         this->_s_copy= NULL;
+	_sId= str.getId();
+	_sLength= str.getLength();
         if(str._s_copy != NULL){
-                _s_copy= new char [16]; //Dynamic memory alocation
+                _s_copy= new char [_sLength]; //Dynamic memory alocation
                 strcpy(_s_copy, str._s_copy);
         }
-
-
 }
 
 Constructor& Constructor::operator=(const Constructor& str){
@@ -26,7 +51,9 @@ Constructor& Constructor::operator=(const Constructor& str){
                 return *this;
         }
         else{
-                char* tmp_s_copy = new char[16];
+		_sId= str.getId();
+		_sLength= str.getLength();
+                char* tmp_s_copy = new char[_sLength];
                 delete []_s_copy;
                 _s_copy= tmp_s_copy;
                 strcpy(_s_copy,str._s_copy);
@@ -39,5 +66,19 @@ Constructor::~Constructor(){
         if(_s_copy != NULL){
                 delete []_s_copy;
         }
+
+}
+
+char* Constructor::getString() const{
+	return _s_copy;
+}
+
+char Constructor::getId() const{
+	return _sId;
+
+
+}
+int Constructor::getLength() const{
+	return _sLength;
 
 }
